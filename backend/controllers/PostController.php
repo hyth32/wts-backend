203cfd60e6b56150bbf20b5f63fef5caec4e1dd0
hyth32 <5474n.sage@gmail.com>
2024-09-18 14:32:49 +0300

    добавил админку постов

--- backend/controllers/PostController.php
+++ backend/controllers/PostController.php
@@ -7,10 +7,36 @@ use yii\rest\Controller;
 use common\models\Post;
 use common\models\AccessToken;
 use yii\web\Response;
+use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
 
 class PostController extends Controller
 {
-    public function actionPublish()
+	 /**
+     * @inheritDoc
+     */
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'delete' => ['POST'],
+						'create' => ['POST'],
+                        'update' => ['PUT'],
+                        'delete' => ['DELETE'],
+                        'view' => ['GET'],
+                        'index' => ['GET'],
+                    ],
+                ],
+            ]
+        );
+    }
+
+    public function actionCreate()
     {
 	Yii::$app->response->format = Response::FORMAT_JSON;
 
@@ -133,5 +159,96 @@ class PostController extends Controller
 			'data' => $serializedPosts,
 		];
 	}
+
+	/**
+     * Lists all Post models.
+     *
+     * @return string
+     */
+	 public function actionIndex()
+    {
+		Yii::$app->response->format = Response::FORMAT_HTML;
+        $dataProvider = new ActiveDataProvider([
+            'query' => Post::find(),
+            /*
+            'pagination' => [
+                'pageSize' => 50
+            ],
+            'sort' => [
+                'defaultOrder' => [
+                    'id' => SORT_DESC,
+                ]
+            ],
+            */
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+	/**
+     * Displays a single Post model.
+     * @param int $id
+     * @return string
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+		Yii::$app->response->format = Response::FORMAT_HTML;
+ 	   return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+	/**
+     * Updates an existing Post model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param int $id
+     * @return string|\yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+	/**
+     * Deletes an existing Post model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param int $id
+     * @return \yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+	/**
+     * Finds the Post model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param int $id
+     * @return Post the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Post::findOne(['id' => $id])) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
 }
 

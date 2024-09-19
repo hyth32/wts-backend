48ca6662b50575cc5a86c6d6f1baf062741ffd6b
hyth32 <5474n.sage@gmail.com>
2024-09-20 00:39:34 +0300

    перенес params в отдельный файл

--- backend/config/main.php
+++ backend/config/main.php
@@ -57,30 +57,5 @@ return [
             ],
         ],
     ],
-    'params' => array_merge($params, [
-        'controllerBehaviors' => [
-            'verbs' => [
-                'class' => VerbFilter::class,
-                'actions' => [
-                    'create' => ['POST'],
-                    'update' => ['PUT'],
-                    'delete' => ['POST'],
-                    'view' => ['GET'],
-                    'index' => ['GET'],
-                ],
-            ],
-            'access' => [
-                'class' => AccessControl::class,
-                'only' => ['index', 'view', 'update', 'delete'],
-                'rules' => [
-                    [
-                        'allow' => true,
-                        'roles' => ['@'],
-                        'matchCallback' => fn() => Yii::$app->user->identity->isAdmin(),
-                    ],
-                    ['allow' => false],
-                ],
-            ],
-        ],
-    ]),
+    'params' => $params,
 ];
--- backend/config/params.php
+++ backend/config/params.php
@@ -1,4 +1,32 @@
 <?php
+
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+
 return [
     'adminEmail' => 'admin@example.com',
+    'controllerBehaviors' => [
+        'verbs' => [
+            'class' => VerbFilter::class,
+            'actions' => [
+                'create' => ['POST'],
+                'update' => ['PUT'],
+                'delete' => ['POST'],
+                'view' => ['GET'],
+                'index' => ['GET'],
+            ],
+        ],
+        'access' => [
+            'class' => AccessControl::class,
+            'only' => ['index', 'view', 'update', 'delete'],
+            'rules' => [
+                [
+                    'allow' => true,
+                    'roles' => ['@'],
+                    'matchCallback' => fn() => Yii::$app->user->identity->isAdmin(),
+                ],
+                ['allow' => false],
+            ],
+        ],
+    ],
 ];
